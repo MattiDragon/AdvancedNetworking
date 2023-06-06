7f3b3ce552028c7d6cad98ef784fefda7e94069f
MattiDragon <61198884+MattiDragon@users.noreply.github.com>
2023-06-06 19:07:10 +0300

    Abstract out filtering logic

--- src/main/java/io/github/mattidragon/advancednetworking/graph/node/fluid/filter/FilterFluidNode.java
+++ src/main/java/io/github/mattidragon/advancednetworking/graph/node/fluid/filter/FilterFluidNode.java
@@ -2,24 +2,21 @@ package io.github.mattidragon.advancednetworking.graph.node.fluid.filter;
 
 import io.github.mattidragon.advancednetworking.graph.ModDataTypes;
 import io.github.mattidragon.advancednetworking.graph.ModNodeTypes;
-import io.github.mattidragon.advancednetworking.graph.node.base.FilterResourceNode;
+import io.github.mattidragon.advancednetworking.graph.node.base.FilterNode;
 import io.github.mattidragon.advancednetworking.graph.node.fluid.FluidTransformer;
 import io.github.mattidragon.advancednetworking.graph.path.PathBundle;
 import io.github.mattidragon.nodeflow.graph.Graph;
 import io.github.mattidragon.nodeflow.graph.data.DataType;
 import net.fabricmc.fabric.api.transfer.v1.fluid.FluidVariant;
 import net.fabricmc.fabric.api.transfer.v1.storage.Storage;
-import net.minecraft.nbt.NbtCompound;
-import net.minecraft.util.Identifier;
+import net.minecraft.fluid.Fluid;
 import net.minecraft.util.registry.Registry;
-import net.minecraft.util.registry.RegistryEntry;
-import org.jetbrains.annotations.Nullable;
 
 import java.util.function.Predicate;
 
-public class FilterFluidNode extends FilterResourceNode<FluidVariant, FluidTransformer> {
+public class FilterFluidNode extends FilterNode<Fluid, FluidVariant, FluidTransformer> {
     public FilterFluidNode(Graph graph) {
-        super(ModNodeTypes.FILTER_FLUID, graph);
+        super(ModNodeTypes.FILTER_FLUID, graph, Registry.FLUID);
     }
 
     @Override
@@ -28,28 +25,7 @@ public class FilterFluidNode extends FilterResourceNode<FluidVariant, FluidTrans
     }
 
     @Override
-    protected @Nullable NbtCompound getNbt(FluidVariant resource) {
-        return resource.getNbt();
-    }
-
-    @Override
-    protected Identifier getId(FluidVariant resource) {
-        return Registry.FLUID.getId(resource.getFluid());
-    }
-
-    @Override
     protected FluidTransformer createTransformer(Predicate<FluidVariant> predicate) {
         return new FluidTransformer.Filter(predicate);
-    }
-
-    @SuppressWarnings("deprecation")
-    @Override
-    protected RegistryEntry<?> getRegistryEntry(FluidVariant resource) {
-        return resource.getFluid().getRegistryEntry();
-    }
-
-    @Override
-    protected Registry<?> getRegistry() {
-        return Registry.FLUID;
     }
 }
