3e637dba9d323952510ada72d7f078d79ff72ad7
MattiDragon <61198884+MattiDragon@users.noreply.github.com>
2023-06-29 11:11:13 +0300

    Fix configtoolkit usage

--- src/main/java/io/github/mattidragon/advancednetworking/config/ConfigData.java
+++ src/main/java/io/github/mattidragon/advancednetworking/config/ConfigData.java
@@ -10,7 +10,7 @@ import net.fabricmc.fabric.api.transfer.v1.fluid.FluidConstants;
 import java.util.function.Function;
 
 @GenerateMutable
-public record ConfigData(int controllerTickRate, long controllerFluidTransferRate, long controllerItemTransferRate, long controllerEnergyTransferRate, boolean disableRegexFilter) {
+public record ConfigData(int controllerTickRate, long controllerFluidTransferRate, long controllerItemTransferRate, long controllerEnergyTransferRate, boolean disableRegexFilter) implements MutableConfigData.Source {
     public static final ConfigData DEFAULT = new ConfigData(10, FluidConstants.BUCKET, 64, 256, false);
     public static final Codec<ConfigData> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             DefaultedFieldCodec.of(Codec.intRange(0, 120), "controller_tick_rate", DEFAULT.controllerTickRate).forGetter(ConfigData::controllerTickRate),
@@ -19,10 +19,6 @@ public record ConfigData(int controllerTickRate, long controllerFluidTransferRat
             DefaultedFieldCodec.of(longRange(256 * 100), "controller_energy_transfer_rate", DEFAULT.controllerEnergyTransferRate).forGetter(ConfigData::controllerEnergyTransferRate),
             DefaultedFieldCodec.of(Codec.BOOL, "disable_regex_filtering", DEFAULT.disableRegexFilter).forGetter(ConfigData::disableRegexFilter)
     ).apply(instance, ConfigData::new));
-
-    public MutableConfigData toMutable() {
-        return new MutableConfigData(this);
-    }
 
     private static Codec<Long> longRange(long maxInclusive) {
         var check = rangeCheck(maxInclusive);
